987a3d3c288780b0d1e61aeabc2052d1e7fca42d
Eduard Mititelu <emititelu-ext@pentalog.com>
2023-01-08 13:20:52 +0200

    Initial Commit

---
+++ AMEFScanner/AMEFScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMEFScannerApp.swift
+//  AMEFScanner
+//
+//  Created by Eduard Mititelu on 08.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct AMEFScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
